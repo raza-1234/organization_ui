75b4c4879d3bc5fbb30911471df9386139d86fdb
raza-1234 <razabutt1863@gmail.com>
2024-03-19 11:46:36 -0700

    button component

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "../../css/Button.css";
+
+type ParentProp = {
+  value: string;
+  clickHandler: () => void;
+  className?: string;
+}
+
+const Button = ({value, clickHandler, className}: ParentProp) => {
+  return (
+    <div>
+      <button
+        className={`custom-button ${className}`}
+        onClick={clickHandler}
+      >
+        {value}
+      </button>
+    </div>
+  )
+}
+
+export default Button
+
