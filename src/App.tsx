09971819a1469333821fa2911dd5730f4c43b3ce
raza-1234 <razabutt1863@gmail.com>
2024-03-25 10:16:04 -0700

    create a component for missing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
 
-import { Routes, Route, useActionData } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import PrivateRoutes from './components/utils/PrivateRoutes';
 import Asset from './components/Asset';
 import UseAuthData from './contexts/authContext';
+import Missing from './components/Missing';
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
           <Route path='/asset-library' element = {<Asset/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
         </Route>
+        <Route path='*' element={<Missing/>}/>
       </Routes>
     </div>
   );
---
+++ src/components/Missing.tsx
@@ -0,0 +1,15 @@
+import "../css/Missing.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Missing = () => {
+  return (
+    <div className='bloowatch-404'>
+      <h1>404</h1>
+      <h3>page not found.</h3> 
+      <Link to='/'><p>go to our website.</p></Link>
+    </div>
+  )
+}
+
+export default Missing
